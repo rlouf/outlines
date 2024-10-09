7c77726f795386a5fa0b49aa37446ea68c294946
Rémi Louf <remi@thetypicalset.com>
2024-10-09 18:35:34 +0200

    Support Anthropic vision models

--- outlines/models/anthropic.py
+++ outlines/models/anthropic.py
@@ -1,4 +1,10 @@
 """Integration with Anthropic's API."""
+import base64
+from io import BytesIO
+from typing import Union
+
+from .openai import Vision
+
 __all__ = ["Anthropic"]
 
 
@@ -9,12 +15,32 @@ class Anthropic:
         self.client = Anthropic(*args, **kwargs)
         self.model_name = model_name
 
-    def __call__(self, prompt: str, max_tokens=1024, **inference_kwargs):
+    def __call__(self, input: Union[str, Vision], max_tokens=1024, **inference_kwargs):
+        if isinstance(input, Vision):
+            image = input.image
+            buffer = BytesIO()
+            image.save(buffer, format=image.format)
+            image_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            image_format = f"image/{image.format.lower()}"
+            content = [
+                {
+                    "type": "image",
+                    "source": {
+                        "type": "base64",
+                        "media_type": image_format,
+                        "data": image_str,
+                    },
+                },
+                {"type": "text", "text": input.prompt},
+            ]
+        else:
+            content = input
+
         completion = self.client.messages.create(
             messages=[
                 {
                     "role": "user",
-                    "content": prompt,
+                    "content": content,
                 }
             ],
             model=self.model_name,
--- tests/models/test_anthropic.py
+++ tests/models/test_anthropic.py
@@ -1,6 +1,11 @@
+import io
+
+import PIL
 import pytest
+import requests
 
 from outlines.models.anthropic import Anthropic
+from outlines.models.openai import Vision
 
 MODEL_NAME = "claude-3-haiku-20240307"
 
@@ -20,4 +25,17 @@ def test_anthropic_wrong_inference_parameters():
 def test_anthropic_simple_call():
     model = Anthropic(MODEL_NAME)
     result = model("Respond with one word. Not more.")
+    assert isinstance(result, str)
+
+
+@pytest.mark.api_call
+def test_anthropic_simple_vision():
+    model = Anthropic(MODEL_NAME)
+
+    url = "https://raw.githubusercontent.com/dottxt-ai/outlines/refs/heads/main/docs/assets/images/logo.png"
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        image = PIL.Image.open(io.BytesIO(r.content))
+
+    result = model(Vision("What does this logo represent?", image))
     assert isinstance(result, str)
